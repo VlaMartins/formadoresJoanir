dc6b6239104bd80c80cd9300c070b69a2b873161
VladismaraMartins <100156223+VlaMartins@users.noreply.github.com>
2025-04-08 16:24:05 -0300

    2

--- script.js
+++ script.js
@@ -26,16 +26,11 @@ botaoDeAcessibilidade.addEventListener('click', function(){
     
 document.addEventListener('DOMContentLoaded', function () {
     //Código fonte seguinte ocultado no slide
-
     const alternaContraste = document.getElementById('alterna-contraste')
     alternaContraste.addEventListener('click', function () {
         document.body.classList.toggle('alto-contraste')
     })
-
     //Código fonte seguinte ocultado no slide
 })
-
-
-
 
 })
