c87c7b25d676d53e7eea460fcf9238a8e1a01a61
VladismaraMartins <100156223+VlaMartins@users.noreply.github.com>
2025-04-08 16:39:02 -0300

    1

--- script.js
+++ script.js
@@ -6,11 +6,6 @@ document.addEventListener('DOMContentLoaded', function(){
     aumentaFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte += 0.1;
         document.body.style.fontSize= `${tamanhoAtualFonte}rem`;
-
-    const alternaContraste = document.getElementById('alterna-contraste')
-        alternaContraste.addEventListener('click', function () {
-            document.body.classList.toggle('alto-contraste')
-        }) //Código fonte seguinte ocultado no slide
     })
 
     // ****************************************************************
@@ -27,6 +22,11 @@ const opcoesDeAcessibilidade=document.getElementById('opcoes-acessibilidade');
 botaoDeAcessibilidade.addEventListener('click', function(){
     botaoDeAcessibilidade.classList.toggle('rotacao-botao');
     opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+
+    const alternaContraste = document.getElementById('alterna-contraste')
+        alternaContraste.addEventListener('click', function () {
+            document.body.classList.toggle('alto-contraste')
+        }) //Código fonte seguinte ocultado no slide
 })
     
 
